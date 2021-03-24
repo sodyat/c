47f7baec056ada69f138b96478f8aa4acc5e1032
yat <sodamyat31@gmail.com>
2021-03-24 14:08:21 +0300

    add

--- first.c
+++ first.c
@@ -236,14 +236,26 @@
 // }
 
 
-int main() {
-  int x = 0;
-  int y = 0;
-  while(x< 5) {
-    y = x - y;
-  printf("%i%i",x,y );
-  x = x + 1;
-  }
-  return 0;
-}
+//int main() {
+//  int x = 0;
+//  int y = 0;
+//  while(x< 5) {
+//    y = x - y;
+//  printf("%i%i",x,y );
+//  x = x + 1;
+//  }
+//  return 0;
+//}
+//
 
+
+
+int main() {
+int x = 0;
+int y = 1;
+while(x < y) {
+printf("%i",x);
+x++;
+}
+return 0;
+}
